19cb06a2d641cd97ac2881cadc9a3063c7ffb181
theishita <ishijaju@gmail.com>
2019-07-08 00:17:08 +0530

    Added numbering functionalities

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var rects = [];
 
 for(var i=0;i<450;i+=30) {
   for(var j=0;j<450;j+=30) {
-    var obj = {x: i, y: j, w: 30, h: 30, col: 'white', hovercol:'lightgrey'};
+    var obj = {x: j, y: i, w: 30, h: 30, col: 'white', hovercol:'lightgrey'};
     rects.push(obj)
   }
 }
@@ -11,12 +11,28 @@ for(var i=0;i<450;i+=30) {
 var _i,_b;
 
 function renderBoard(hover, id) {
+  var num = 1;
   for(_i=0;_b=rects[_i];_i++ ) {
-    console.log(hover,id,_i)
+    // console.log(hover,id,_i)
     cx.fillStyle = (hover && id === _i) ? _b.hovercol:_b.col;
     cx.fillRect(_b.x,_b.y,_b.w,_b.h);
     cx.lineWidth = 2;
     cx.strokeRect(_b.x,_b.y,_b.w,_b.h)
+
+    cx.font = "10px Arial";
+    cx.fillStyle = "black"
+    if(_b.col != "black"){
+      // console.log(rects[_i].x/30)
+      if(rects[_i-1] && rects[_i-1].col == "black") {
+        cx.fillText(num++,_b.x + 2,_b.y + 9.5)
+      }
+      else if(rects[_i-15] && rects[_i-15].col == "black") {
+        cx.fillText(num++,_b.x + 2,_b.y + 9.5)
+      }
+      else if(_b.x/30 == 0 || _b.y/30 == 0) {
+        cx.fillText(num++,_b.x + 2,_b.y + 9.5)
+      }
+    }
   }
 }
 
@@ -33,7 +49,7 @@ canvas.onmousemove = function(e) {
   for(var i=rects.length - 1,b;b = rects[i]; i--) {
     if(x >= b.x && x <= b.x + b.w &&
        y >= b.y && y <= b.y + b.h) {
-        console.log(x,y)
+        // console.log(x,y)
         hover = true;
         id = i;
         break;
@@ -55,7 +71,7 @@ canvas.addEventListener('contextmenu',function(e) {
     if(x >= b.x && x <= b.x + b.w &&
        y >= b.y && y <= b.y + b.h) {
 
-        console.log(x,y)
+        // console.log(x,y)
 
         if(rects[i].col == 'black') {
           rects[i].col = 'white'
